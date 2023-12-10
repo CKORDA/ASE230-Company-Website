63475299f587a67ee95d060fc532690144da16e2
CKORDA <144379496+CKORDA@users.noreply.github.com>
2023-12-10 17:34:27 -0500

    Update and rename getPlainText.php to readPlainText.php

--- getPlainText.php
+++
@@ -1,7 +0,0 @@
-//this is the function page
-
-<?php
-	function getPlainText(){
-	<p class="mb-5 text-muted subtitle w-75 mx-auto">file_get_contents('plainText.txt');</p>
-	}
-                            
